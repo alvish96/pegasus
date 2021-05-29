4f9ccf18f8ae49030d41efe3c2bd9ad114d21cc7
Vishal Singh <Vishal Singh@192.168.1.3>
2021-05-29 16:48:55 +0530

    first commit
    all files

---
+++ workone/src/workone/firstclass.java
@@ -0,0 +1,16 @@
+package workone;
+
+public class firstclass 
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		System.out.println(a);
+		
+		
+		System.out.println("Hello world");
+		
+	System.out.println();
+	}
+
+}
